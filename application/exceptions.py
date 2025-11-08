f9298ac5d6c4303480ad572a6a5c91e86d38ffd3
mabagit config --global user.name maba <gyj0149@gmail.com>
2025-11-08 22:32:49 +0900

    feat(application): APIKeyService 관련 예외 클래스 추가

---
+++ application/exceptions.py
@@ -0,0 +1,13 @@
+class APIKeyServiceError(Exception):
+    """APIKeyService 관련 예외 클래스
+
+    Args:
+        Exception (_type_): _description_
+    """
+
+    def __init__(self, message: str):
+        super().__init__(message)
+        self.message = message
+
+    def __str__(self):
+        return f"APIKeyServiceError: {self.message}"
