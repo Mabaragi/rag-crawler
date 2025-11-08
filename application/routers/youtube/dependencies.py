3a5973c726db02160c1b5068fd4f2d94f6a07127
mabagit config --global user.name maba <gyj0149@gmail.com>
2025-11-08 22:31:36 +0900

    DI: YouTube 관련 의존성에 APIKeyService 및 MongoAPIKeyRepository 추가

--- application/routers/youtube/dependencies.py
+++ application/routers/youtube/dependencies.py
@@ -1,6 +1,6 @@
-from application.services.youtube_service import ChannelInsertService
+from application.services.youtube_service import APIKeyService, ChannelInsertService
 from infrastructure.api.youtube_api_client import YoutubeAPIClient
-from infrastructure.persistence.mongo_repository import MongoYoutubeRepository
+from infrastructure.persistence.mongo_repository import MongoAPIKeyRepository, MongoYoutubeRepository
 from shared.config.settings import get_settings
 
 
@@ -9,3 +9,8 @@ def get_channel_insert_service() -> ChannelInsertService:
     mongo_repo = MongoYoutubeRepository()
     youtube_client = YoutubeAPIClient(api_key=settings.YOUTUBE_API_KEY)
     return ChannelInsertService(mongo_repo, youtube_client)
+
+
+def get_api_key_service() -> APIKeyService:
+    mongo_repo = MongoAPIKeyRepository()
+    return APIKeyService(mongo_repo)
