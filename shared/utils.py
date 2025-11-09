31dd890054e5c957ec6a9c85d982e75a6d4b31c7
mabagit config --global user.name maba <gyj0149@gmail.com>
2025-11-09 14:35:42 +0900

    shared: add dataclass-from-dict utility and imports

--- shared/utils.py
+++ shared/utils.py
@@ -1,4 +1,6 @@
 import datetime
+from dataclasses import fields, is_dataclass
+from typing import Any, Type, TypeVar
 
 YOUTUBE_API_RESET_HOUR = 7
 YOUTUBE_API_QUOTA_LIMIT = 10000
@@ -36,3 +38,26 @@ def is_quota_exceeded(quota_used: int, updated_at: datetime.datetime) -> bool:
     if is_quota_reseted(updated_at):
         return False
     return quota_used >= YOUTUBE_API_QUOTA_LIMIT
+
+
+T = TypeVar("T")
+
+
+def data_class_from_dict(cls: Type[T], data: dict[str, Any]) -> T:
+    """딕셔너리로 부터 데이터클래스 인스턴스를 생성하는 유틸리티 함수
+
+    Args:
+        cls (Type[T]): _description_
+        data (dict[str, Any]): _description_
+
+    Raises:
+        ValueError: _cls_ 가 데이터클래스가 아닐 경우
+
+    Returns:
+        T: _cls_ 타입의 데이터클래스 인스턴스
+    """
+    if not is_dataclass(cls):
+        raise ValueError(f"{cls} is not a dataclass")
+    valid_keys = [field.name for field in fields(cls)]
+    data = {k: v for k, v in data.items() if k in valid_keys}
+    return cls(**data)
